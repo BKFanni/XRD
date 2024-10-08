9fc698b56c4f6a3b91ba29a54e64f78407534455
Andrejs Dāvis Umbrovskis <ledaviskylv@gmail.com>
2024-10-08 14:20:06 +0200

    Destinations

--- XRD-AR2/Assets/Scripts/DestinationOptions.cs
+++ XRD-AR2/Assets/Scripts/DestinationOptions.cs
@@ -2,40 +2,52 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+public enum Destination
+{
+    Canteen,
+    Reception,
+    ElevatorA,
+    ElevatorB,
+    ElevatorC,
+    ElevatorMain
+}
+
 public class DestinationManager : MonoBehaviour
 {
     public Button canteenButton;
     public Button receptionButton;
-    public Button elevatorsButton;
-    public Button toiletsButton;
     public Button exitButton;
+
+    public Button elevatorAButton;
+    public Button elevatorBButton;
+    public Button elevatorCButton;
+    public Button elevatorMainButton;
 
     void Start()
     {
-        canteenButton.onClick.AddListener(() => OnDestinationSelected("Canteen"));
-        receptionButton.onClick.AddListener(() => OnDestinationSelected("Reception"));
-        elevatorsButton.onClick.AddListener(() => OnDestinationSelected("Elevators"));
-        toiletsButton.onClick.AddListener(() => OnDestinationSelected("Toilets"));
+        //canteenButton.onClick.AddListener(() => OnDestinationSelected("Canteen"));
+        //receptionButton.onClick.AddListener(() => OnDestinationSelected("Reception"));
+        //elevatorAButton.onClick.AddListener(() => OnDestinationSelected("ElevatorA"));
+        //elevatorBButton.onClick.AddListener(() => OnDestinationSelected("ElevatorB"));
+        //elevatorCButton.onClick.AddListener(() => OnDestinationSelected("ElevatorC"));
+        //elevatorMainButton.onClick.AddListener(() => OnDestinationSelected("ElevatorMain"));
         exitButton.onClick.AddListener(ExitApplication);
     }
 
-    public void OnDestinationSelected(string destinationName)
+    public void OnDestinationSelected(Destination destinationName)
     {
         Debug.Log("Selected destination: " + destinationName);
 
         switch (destinationName)
         {
-            case "Canteen":
+            case Destination.Canteen:
                 Debug.Log("Loading AR content for the Canteen...");
                 break;
-            case "Reception":
+            case Destination.Reception:
                 Debug.Log("Loading AR content for the Reception...");
                 break;
-            case "Elevators":
+            case Destination.ElevatorA:
                 Debug.Log("Loading AR content for the Elevators...");
-                break;
-            case "Toilets":
-                Debug.Log("Loading AR content for the Toilets...");
                 break;
             default:
                 Debug.Log("Unknown destination");
